22b77c3787642e770b4e4c4a36bf2eba7745f491
Hafeez <izzhafeez@gmail.com>
2023-08-17 13:58:31 +0800

    Edited ./reader

--- src/reader/file_reader.rs
+++ src/reader/file_reader.rs
@@ -1,6 +1,14 @@
+use std::collections::HashMap;
 use std::fs;
 use std::path::PathBuf;
 
 pub fn read_file(path: PathBuf) -> String {
     fs::read_to_string(path).expect("No such file")
+}
+
+pub fn read_all_files(paths: Vec<PathBuf>) -> Vec<String> {
+    paths
+        .into_iter()
+        .map(read_file)
+        .collect::<Vec<String>>()
 }
