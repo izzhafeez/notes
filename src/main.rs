9ed6cbe498c7ad0c9f5fa37228c38aabd2d7af30
Hafeez <izzhafeez@gmail.com>
2023-08-17 13:58:31 +0800

    Edited ./main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
+use crate::document::document::Document;
 use crate::reader::directory_reader::loop_directory;
-use crate::reader::file_reader::read_file;
+use crate::reader::file_reader::{read_all_files, read_file};
 
 mod document;
 mod reader;
 
 fn main() -> Result<(), ()> {
     let files = loop_directory()?;
-    for (k, v) in files {
-        println!("{} {:?}", k, v.into_iter().map(read_file).collect::<Vec<String>>());
-    }
+    let documents = files
+        .into_iter()
+        .map(|(k, v)| v)
+        .map(read_all_files)
+        .map(|v| v
+            .into_iter()
+            .map(Document::parse)
+            .map(|s| s.unwrap().to_string())
+            .collect::<Vec<String>>()
+            .join("\n\n\n"))
+        .collect::<Vec<String>>()
+        .join("\n\n\n\n");
+
+    println!("{}", documents);
 
     Ok(())
 }
