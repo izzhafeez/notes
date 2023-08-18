fec7ce6146f0d27668f446238426568342a78445
Hafeez <izzhafeez@gmail.com>
2023-08-18 11:01:29 +0800

    Edited ./writer

---
+++ src/writer/writer.rs
@@ -0,0 +1,14 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::Write;
+use crate::document::document::Document;
+
+pub fn write_json(documents: HashMap<String, Vec<Document>>) -> Result<(), ()> {
+    let mut file: File = File::create("./processed/notes.json").unwrap();
+    // let mut writer = BufWriter::new(file);
+    let json_string: String = serde_json::to_string(&documents).unwrap();
+    file.write(json_string.as_ref()).expect("Write failed.");
+    println!("{}", json_string);
+
+    Ok(())
+}
