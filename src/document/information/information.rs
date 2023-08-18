eae5253eb061b5b12a85bceca204f2d58db6a3de
Hafeez <izzhafeez@gmail.com>
2023-08-18 11:00:57 +0800

    Edited ./information.rs

--- src/document/information/information.rs
+++ src/document/information/information.rs
@@ -1,28 +1,31 @@
 use std::fmt::{Display, Formatter};
+use serde::{Serialize};
+use crate::document::information::category::category::get_category;
 
-use crate::document::information::parser::parser::{Parser, get, parse};
-
+#[derive(Serialize)]
 pub struct Information {
+    category: String,
     text: String
 }
 
 impl Information {
-    pub fn new(s: &str) -> Self {
-        let text: String = s.parse().unwrap();
-        Self { text }
-    }
-
-    fn get_parser(&self) -> Result<Parser, ()> {
-        let first_letter = self.text
+    pub fn parse(s: &str) -> Result<Self, ()> {
+        let text: String = s[3..].parse().unwrap();
+        let first_letter = s
             .chars()
             .nth(0)
-            .unwrap();
-        get(first_letter)
+            .expect("Line is blank!");
+        let category: String = get_category(first_letter)
+            .expect(&format!("Invalid character token: {first_letter}"));
+        Ok(Self { category, text })
     }
 
-    fn parse(&self) -> Result<String, ()> {
-        let parser: Parser = self.get_parser()?;
-        parse(parser, &self.text)
+    pub fn get_category(&self) -> String {
+        self.category.to_string()
+    }
+
+    pub fn get_text(&self) -> String {
+        self.text.to_string()
     }
 }
 
