4c4e13a7ded46f8d79449168474fd0c739c40c72
Hafeez <izzhafeez@gmail.com>
2023-08-18 11:17:15 +0800

    Remove unnecessary from writer

--- src/writer/writer.rs
+++ src/writer/writer.rs
@@ -5,10 +5,9 @@ use crate::document::document::Document;
 
 pub fn write_json(documents: HashMap<String, Vec<Document>>) -> Result<(), ()> {
     let mut file: File = File::create("./processed/notes.json").unwrap();
-    // let mut writer = BufWriter::new(file);
     let json_string: String = serde_json::to_string(&documents).unwrap();
     file.write(json_string.as_ref()).expect("Write failed.");
-    println!("{}", json_string);
+    println!("{:?}", json_string);
 
     Ok(())
 }
